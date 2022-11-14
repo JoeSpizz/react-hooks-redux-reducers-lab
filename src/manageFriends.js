2d8f4b641aad40a6d957e6615894ffad7f9ee10b
Joe <spizjo01@gmail.com>
2022-11-14 14:05:30 -0700

    commit tree d2e684ff39f355ea9b2e64fe3856f5111f00489e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
 export function manageFriends(state, action) {
-  // your code here
+  // your code her
+    switch(action.type){
+        case "friends/add":
+          return {friends : [...state.friends, action.payload]};
+        case "friends/remove":
+          let friends= state.friends.filter(friend => friend.id !== action.payload);
+          return {friends: friends}
+        default:
+          return state
+    }
+    
+  
 }
